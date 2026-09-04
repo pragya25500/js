2f333d4c4c7813ede3666d627c783b7422d85cf8
Pragya <pragyadiptasaha@gmail.com>
2026-09-05 00:41:33 +0530

    coversion

--- 1-basics/3-conversion.js
+++ 1-basics/3-conversion.js
@@ -22,3 +22,34 @@ let someNumber = 23
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//* */
+
+
+let val=5
+let negval=-val
+console.log(negval);
+
+let str1="hi"
+let str2="rin"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+
+let n1,n2,n3
+n1=n2=n3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
