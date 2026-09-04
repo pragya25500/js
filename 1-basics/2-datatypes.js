c98413a208b5bc5f3bfcd779cc433d59335cc64a
Pragya <pragyadiptasaha@gmail.com>
2026-09-04 23:43:04 +0530

    datatypes

---
+++ 1-basics/2-datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treats all js code as newer version
+
+console.log(3
+    +
+    3)
+
+console.log("rin")
+
+let name="rin"
+let age = 19
+let isLoggedIn = true
+let state;
+
+console.log(typeof state);
