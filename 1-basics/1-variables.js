b9d64bd7d300dc93a91696bbd5f2ee5e76ef9992
Pragya <pragyadiptasaha@gmail.com>
2026-09-04 20:06:11 +0530

    Added new files and updates

---
+++ 1-basics/1-variables.js
@@ -0,0 +1,13 @@
+const accoundId = 25500
+let accountEmail = "rin@gmail.com"
+var accountPassword = "12345"
+accounCity = "Kolkata"
+let accountState;
+
+//accoundId = 5
+accountEmail = "pragya@gmail.com"
+accountPassword = "23456"
+accounCity = "delhi"
+console.log(accoundId);
+
+console.table([accoundId,accountEmail,accountPassword,accounCity,accountState])
