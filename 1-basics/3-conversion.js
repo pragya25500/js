d683722d9e3013e63e3a019d34df076b16d54789
Pragya <pragyadiptasaha@gmail.com>
2026-09-05 00:18:59 +0530

    conversion

---
+++ 1-basics/3-conversion.js
@@ -0,0 +1,24 @@
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valinnum = Number(score)
+console.log(typeof valinnum);
+console.log(valinnum);
+
+//null=0
+//true=1
+//false=0
+//"23"=23
+//"23a"=NaN
+
+let isLoggedIn = "rin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 23
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
