b4ce18327dc748348343ebcf9238e9cf4cea1ee8
Pragya <pragyadiptasaha@gmail.com>
2026-09-05 17:06:20 +0530

    stack heap

---
+++ 1-basics/stackheapmemory.js
@@ -0,0 +1,21 @@
+let ytname = "rin"
+let anothername = ytname
+anothername="pragya"
+console.log(ytname);
+console.log(anothername);
+
+let user1={
+    email:"rin@gmail.com"
+}
+
+
+let user2=user1
+
+user2.email="pragya@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+//stack=primitive
+//heap=non primitive references
